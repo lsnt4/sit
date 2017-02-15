c14ab62ccd2acaf98f5b41a628953db45164913f
Lasantha <lasanthabuddhika@gmail.com>
2017-02-15 14:23:19 +0530

    Font colors and header files

---
+++ y1/s1/ip-it1010/w1_selfStudy_colors.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "w1_selfStudy_colors.h"
+
+int main() {
+	printf(ANSI_COLOR_RED		"RED "		ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GREEN		"GREEN "	ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_YELLOW	"YELLOW "	ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE		"BLUE "		ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_MAGENTA	"MAGENTA "	ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_CYAN		"CYAN "		ANSI_COLOR_RESET "\n");
+	return 0;
+}
+
+/*
+ * References:
+ */
+
+// Colors - http://stackoverflow.com/a/3219471/3683435
+// Color codes - https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
