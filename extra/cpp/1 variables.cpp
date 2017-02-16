662077d1b1c36d9a2c9c8bed8874b7cf45e8b19c
Lasantha <lasanthabuddhika@gmail.com>
2017-02-17 00:57:41 +0530

    cpp variables

---
+++ extra/cpp/1 variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	std::system("clear");
+
+	float i, j, k;
+
+	i = 34.545456456;
+	j = 2.153;
+	k = i/j;
+
+	cout << k << endl;
+}
