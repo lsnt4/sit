3baa9ab8254c24002f8761d63cf8803b309b5361
Lasantha <lasanthabuddhika@gmail.com>
2017-03-18 02:12:04 +0530

    4 w6-ip-exam-ex.py

---
+++ extra/python/4 w6-ip-exam-ex.py
@@ -0,0 +1,29 @@
+vehicleType = raw_input("Car (c) or Van (v): ")
+distance = input("Distance? ")
+
+class Taxi :
+	def __init__(self, vehicleType, distance) :
+		self.vehicleType = vehicleType
+		self.distance = distance
+
+	def vehicle(self):
+		if (self.vehicleType == 'c'):
+			first25km = 25.50
+			second25km = 50.00
+		elif (self.vehicleType == 'v'):
+			first25km = 35.5
+			second25km = 55.00
+		else :
+			print "Invalid vehicle type"
+
+		if (self.distance <= 25) :
+			cost = self.distance * first25km
+			print "Your total cost is: %.2f"%(cost)
+		else :
+			self.distance -= 25
+			cost = self.distance * second25km + first25km * 25
+			print "Your total cost is: %.2f"%(cost)
+
+
+hire = Taxi(vehicleType, distance)
+hire.vehicle()
