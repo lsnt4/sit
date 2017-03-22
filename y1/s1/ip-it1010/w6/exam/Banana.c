578d0366c82ac79115a61f7ce08b20292f3e3ec5
Lasantha <lasanthabuddhika@gmail.com>
2017-03-22 19:32:06 +0530

    IP Exam

---
+++ y1/s1/ip-it1010/w6/exam/Banana.c
@@ -0,0 +1,24 @@
+
+/* calculate displacement */
+
+#  include <sdtio.h>
+
+int main(void)
+{
+		int d, v, t, a;
+
+		
+		printf("Please enter a value for velocity (v) ");
+	scanf("%f",&v);
+		printf("Please enter a value for time (t)" );
+	scanf("%f",&t);
+		printf("Please enter a value for accelaration(a)");
+	scanf("%f", a);
+
+	
+	d = vt + 1/2at^2
+
+	/* write output */
+
+	printf("\n The diaplacement (d) is: %f\n ", d);
+}
---
+++ y1/s1/ip-it1010/w6/exam/Lemon.c
@@ -0,0 +1,24 @@
+
+/* perfrom a calculation/*
+
+#  include <stdio>
+
+int main(void)
+{
+		int R, a, t, c;
+
+		
+		printf("Please enter a value for c ");
+	scanf("%f",&c);
+		printf("Please enter a value for t" );
+	scanf("%f",&t);
+		printf("Please enter a value for a");
+	scanf("%f", &a);
+
+	
+R = 1+at/c *2;
+
+	/* write output */
+
+printf("\n The diaplacement (d) is: %f\n ", R);
+}
