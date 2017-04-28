fca22b7cb9b34341f24bd5aa32fe0cec0a6d35f1
Lasantha <lasanthabuddhika@yahoo.com>
2017-04-29 01:16:35 +0530

    w10

--- y1/s1/ip-it1010/w10/ex1.c
+++ y1/s1/ip-it1010/w10/ex1.c
@@ -5,38 +5,24 @@ int factorial(int number);
 int power(int a, int b);
 
 int main(void) {
-
 	printf("%d", power(3, 4) );
-
 	return 0;
 }
 
 int power(int a, int b) {
-
 	if(b == 0) {
 		return 1;
 	} else {
 		return a*power(a, b-1);
 	}
-
 }
 
-
-
-
-
-
-
-
-
 int factorial(int number) {
-
 	if(number <= 1) {
 		return 1;
 	} else {
 		return (number * factorial(number - 1));
 	}
-
 }
 
 int fact(int x) {
